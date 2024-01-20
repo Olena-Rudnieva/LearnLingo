20eed82f80a6cf081af719e9119b9e4dda44108d
Olena-Rudnieva <rudnievao@gmail.com>
2024-01-21 00:05:38 +0200

    Update reload

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
-import { HashRouter } from 'react-router-dom';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import { persistor, store } from './redux/store';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -10,9 +11,9 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <HashRouter basename="/LearnLingo">
+        <BrowserRouter basename="/LearnLingo">
           <App />
-        </HashRouter>
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </React.StrictMode>
